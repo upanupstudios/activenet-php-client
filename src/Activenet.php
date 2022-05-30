daa5bfc3c95b0f4db02baa64cafadc961ef4fba4
Marco Maranao <marco@maranao.ca>
2022-05-30 16:23:55 -0600

    Initial commit

---
+++ src/Activenet.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Upanupstudios\Activenet\Php\Client;
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
+
+class Activenet
+{
+  private $config;
+  private $httpClient;
+
+  private $version = '2.0';
+
+  public function __construct(Config $config, ClientInterface $httpClient)
+  {
+    $this->config = $config;
+    $this->httpClient = $httpClient;
+  }
+
+  public function getConfig(): Config
+  {
+    return $this->config;
+  }
+
+  public function getHttpClient(): ClientInterface
+  {
+    return $this->httpClient;
+  }
+
+  public function status()
+  {
+    try {
+      $request = $this->httpClient->request('GET', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/Status', [
+        'headers' => [
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'X-Client-Number' => $this->getConfig()->getClientNumber(),
+          'X-Access-Key' => $this->getConfig()->getApiKey()
+        ]
+      ]);
+
+      $response = $request->getBody();
+      $response = $response->__toString();
+    } catch (RequestException $exception) {
+      $response = $exception->getMessage();
+    }
+
+    return $response;
+  }
+
+  public function query(string $query)
+  {
+    try {
+      $body = Psr7\Utils::streamFor(json_encode(array('QueryString' => $query)));
+
+      $request = $this->httpClient->request('POST', $this->getConfig()->getApiUrl().'/api/'.$this->version.'/B2C/Query', [
+        'headers' => [
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json',
+          'X-Client-Number' => $this->getConfig()->getClientNumber(),
+          'X-Access-Key' => $this->getConfig()->getApiKey(),
+        ],
+        'body' => $body
+      ]);
+
+      $response = $request->getBody();
+      $response = $response->__toString();
+    } catch (RequestException $exception) {
+      $response = $exception->getMessage();
+    }
+
+    return $response;
+  }
+
+  /**
+   * @return object
+   *
+   * @throws InvalidArgumentException
+   */
+  public function api(string $name)
+  {
+    $api = null;
+
+    switch ($name) {
+      default:
+        throw new \InvalidArgumentException("Undefined api instance called: '$name'.");
+    }
+
+    return $api;
+  }
+
+  public function __call(string $name, array $args): object
+  {
+    try {
+      return $this->api($name);
+    } catch (\InvalidArgumentException $e) {
+      throw new \BadMethodCallException("Undefined method called: '$name'.");
+    }
+  }
+}
---
+++ src/Config.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Upanupstudios\Activenet\Php\Client;
+
+final class Config
+{
+  private $apiUrl;
+  private $clientNumber;
+  private $apiKey;
+
+  public function __construct(string $apiUrl, string $clientNumber, string $apiKey)
+  {
+    $this->apiUrl = $apiUrl;
+    $this->clientNumber = $clientNumber;
+    $this->apiKey = $apiKey;
+  }
+
+  /**
+   * Get API URL.
+   */
+  public function getApiUrl(): string
+  {
+      return $this->apiUrl;
+  }
+
+  /**
+   * Get Client Number.
+   */
+  public function getClientNumber(): string
+  {
+      return $this->clientNumber;
+  }
+
+  /**
+   * Get API Key.
+   */
+  public function getApiKey(): string
+  {
+      return $this->apiKey;
+  }
+}
